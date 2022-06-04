12d80bcacdcc4b85a5cfddc57f2e5dc2be8b5e43
Satyarth Agrahari <satyarthagrahari@gmail.com>
2022-06-04 18:51:27 +0530

    hello world kernel

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@
 
 use core::panic::PanicInfo;
 
+static HELLO: &[u8] = b"Hello World";
+
 #[panic_handler]
 fn panic(_: &PanicInfo) -> ! {
     loop {}
@@ -10,5 +12,15 @@ fn panic(_: &PanicInfo) -> ! {
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+    for (i, &byte) in HELLO.iter().enumerate() {
+        // SAFETY: vga_buffers start at 0xb8000, and each printable char has 2 bytes. 1st for ascii
+        // value, and 2nd for character color.
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
